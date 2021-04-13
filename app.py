d87f69cd46032bad34151c1ca04c3d59c4536864
sathapornma <sathaporn.ma@rmuti.ac.th>
2021-04-14 00:00:16 +0700

    HTTP post method and parameter

--- app.py
+++ app.py
@@ -4,16 +4,35 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+mycity = {
+    1 : {
+        "name" : "chonburi",
+        "weather" : "hot",
+        "people" : 1000,
+    },
+    2 : {
+        "name" : "rayong",
+        "weather" : "very hot",
+        "people" : 2000,
+    },
+    3 : {
+        "name" : "bangkok",
+        "weather" : "cloud",
+        "people" : 3000,
+    },
+} 
 
 class WeatherCity(Resource):
 
-    def get(self):
-        return {"data":"hello"}
+    def get(self, city_id):
+        return mycity[city_id]
+
+    def post(self, city_id):
+        return {"data": "post data : " + str(city_id)}
+        
 
 
-
-
-api.add_resource(WeatherCity,"/weather")
+api.add_resource(WeatherCity,"/weather/<int:city_id>")
 
 
 if __name__ == "__main__":
