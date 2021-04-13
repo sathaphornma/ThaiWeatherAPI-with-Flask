5cfd56852efcbbb0d654e21aca0b240258305d66
sathapornma <sathaporn.ma@rmuti.ac.th>
2021-04-14 01:46:57 +0700

    insert data

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Api, Resource, abort, reqparse
+from flask_restful import Api, Resource, abort, reqparse, marshal_with, fields
 from flask_sqlalchemy import SQLAlchemy, Model
 
 app = Flask(__name__)
@@ -29,42 +29,44 @@ city_add_args.add_argument("temp", type=str, required=True, help="please enter t
 city_add_args.add_argument("weather", type=str, required=True, help="please enter text for weather.")
 city_add_args.add_argument("people", type=str, required=True, help="please enter text for people.")
 
-mycity = {
-    "chonburi" : {
-        "name" : "chonburi",
-        "weather" : "hot",
-        "people" : 1000,
-    },
-    "rayong" : {
-        "name" : "rayong",
-        "weather" : "very hot",
-        "people" : 2000,
-    },
-    "bangkok" : {
-        "name" : "bangkok",
-        "weather" : "cloud",
-        "people" : 3000,
-    },
+# Set Fields
+resource_field = {
+    "id" : fields.Integer,
+    "name" : fields.String,
+    "temp" : fields.String,
+    "weather" : fields.String,
+    "people" : fields.String,
 }
 
 
-def notfoundCity(name):
-    if name not in mycity:
+def notfoundCity(city_id):
+    if city_id not in mycity:
         abort(404, message="province not found.")
 
 class WeatherCity(Resource):
+    @marshal_with(resource_field)
+    def get(self, city_id):
+        notfoundCity(city_id)
+        return mycity[city_id]
 
-    def get(self, name):
-        notfoundCity(name)
-        return mycity[name]
-
-    def post(self, name):
+    @marshal_with(resource_field)
+    def post(self, city_id):
         args = city_add_args.parse_args()
-        return args
+        city = CityModel(
+            id=city_id, 
+            name=args["name"], 
+            temp=args["temp"], 
+            weather=args["weather"], 
+            people=args["people"]
+        )
+
+        db.session.add(city)
+        db.session.commit()
+        return city,201
         
 
 
-api.add_resource(WeatherCity,"/weather/<string:name>")
+api.add_resource(WeatherCity,"/weather/<int:city_id>")
 
 
 if __name__ == "__main__":
