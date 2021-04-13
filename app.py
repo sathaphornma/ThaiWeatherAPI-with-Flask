f4a0e3d55b96e73e0568c7dc3a99133cb377ef74
sathapornma <sathaporn.ma@rmuti.ac.th>
2021-04-13 23:20:18 +0700

    HTTP get method

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask
-from flask_restful import Api
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
+
+
+class WeatherCity(Resource):
+
+    def get(self):
+        return {"data":"hello"}
+
+
+
+
+api.add_resource(WeatherCity,"/weather")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
